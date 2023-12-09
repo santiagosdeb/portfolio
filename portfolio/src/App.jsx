5ca8a8844c2b3869ec7d740646776f3e3cacb2dc
Santiago SdeB <santiagosanchezdeb@gmail.com>
2023-12-08 21:14:16 -0300

    elimino state de App

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,17 +3,16 @@ import Contact from "./Views/Contact";
 import Home from "./Views/Home";
 import Projects from "./Views/Projects";
 import Skills from "./Views/Skills";
-import State from "./context/state.jsx";
 
 function App() {
     return (
-        <State>
-            <NavBar />
-            <Home />
-            <Skills />
-            <Projects />
-            <Contact />
-        </State>
+        <>
+        <NavBar />
+        <Home />
+        <Skills />
+        <Projects />
+        <Contact />
+        </>
     )
 };
 
